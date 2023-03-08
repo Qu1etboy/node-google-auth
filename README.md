501062a1790d91738f7a50796bfea8c8f585964d
Qu1etboy <nonzagreanthai@gmail.com>
2023-03-08 21:27:18 +0700

    feat(*): initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# node-google-auth
+
+A simple example using [google-auth-library](https://www.npmjs.com/package/google-auth-library) to authenticate and authorize users.
+
+## Technologies used
+
+- Node.js
+- Express
+
+## Getting Started
+
+setup `google client id` and `google client secret` in your environment variable.
+which can be found in https://console.cloud.google.com/apis/credentials
+
+- set Authorized JavaScript origins as http://localhost:3000
+
+- set Authorized redirect URIs as http://localhost:3000/auth/google/callback
+
+```
+GOOGLE_CLIENT_ID="your_client_id"
+GOOGLE_CLIENT_SECRET="your_client_secret"
+```
+
+install dependencies
+
+```bash
+npm install
+# or
+pnpm install
+```
+
+start server
+
+```bash
+npm run dev
+# or
+pnpm dev
+```
+
+## Usage
+
+- visit http://localhost:3000/auth/google to login
---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+app.use(cookieParser());
+
+const { OAuth2Client } = require("google-auth-library");
+
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = "http://localhost:3000/auth/google/callback";
+
+const client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const { tokens } = await client.getToken(code);
+
+    client.setCredentials(tokens);
+
+    const { data } = await client.request({
+      url: "https://www.googleapis.com/oauth2/v1/userinfo?alt=json",
+      method: "GET",
+    });
+
+    console.log("google data : ", data);
+
+    // use JWT to create token and sent that token to client and store it in session or cookies
+    const token = jwt.sign(
+      {
+        ...data,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    console.log("here");
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+app.get("/auth/google", (req, res) => {
+  const authUrl = client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["email", "profile"],
+  });
+
+  res.redirect(authUrl);
+});
+
+const jwtAuthMiddleware = (req, res, next) => {
+  const cookies = req.cookies;
+  // console.log("Cookies:", cookies);
+
+  // no cookies or no token provide send status 401 unauthorized
+  if (!cookies || !cookies.token) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  const token = req.cookies.token;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // set request
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.error("Error verifying JWT token:", err);
+    return res.status(400).send(err);
+  }
+};
+
+app.get("/", jwtAuthMiddleware, (req, res) => {
+  // console.log(req.user);
+
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => console.log("Listening at port 3000"));
